6570deb71b55d87d4ab45152b462b44200464cdb
Md Noman Hossain <mdn9707@gmail.com>
2024-08-31 02:38:07 +0600

    Add files via upload

---
+++ Series/S1.c
@@ -0,0 +1,19 @@
+//2+4+6+8......+100
+
+//* Ask for the last terms [eg. 100]
+
+//* Ask for how many numbers of terms? [eg. 50]
+
+
+*Hints*
+
+int sum 0;
+
+for()
+{
+
+       {
+         sum = sum +        ;
+       }
+       printf("%d", sum);
+}
